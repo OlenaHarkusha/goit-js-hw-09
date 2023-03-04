4434d0e6cedf330ecf1e8cff30853fd70b0d0d66
OlenaHarkusha <olenaharkusha26@gmail.com>
2023-03-04 15:36:46 +0200

    add hw1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,34 @@
+refs = {
+  start: document.querySelector('button[data-start]'),
+  stop: document.querySelector('button[data-stop]'),
+};
 
+refs.start.addEventListener('click', onStartbtnClick);
+refs.stop.addEventListener('click', onStopbtnClick);
+
+let timerId = null;
+
+refs.stop.disabled = true;
+refs.start.disabled = false;
+
+function onStartbtnClick() {
+  if (timerId) return;
+
+  refs.stop.disabled = false;
+  refs.start.disabled = true;
+  timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function onStopbtnClick() {
+  refs.stop.disabled = true;
+  refs.start.disabled = false;
+
+  clearInterval(timerId);
+  timerId = undefined;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
